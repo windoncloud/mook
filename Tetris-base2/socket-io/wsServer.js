412864f0f084b310019ed0975642619630736dcd
Kingsley <448409934@qq.com>
2018-05-09 23:33:08 +0800

    idea => .idea，socket.io

--- Tetris-base2/socket-io/wsServer.js
+++ Tetris-base2/socket-io/wsServer.js
@@ -1,8 +1,38 @@
-var ws = require("nodejs-websocket")
-
+// var app = require('http').createServer(handler)
+var app = require('http').createServer()
+var io = require('socket.io')(app);
+// var fs = require('fs');
 var PORT = 3000
-
 var clientCount = 0
+app.listen(PORT);
+
+// function handler (req, res) {
+//     fs.readFile(__dirname + '/index.html',
+//         function (err, data) {
+//             if (err) {
+//                 res.writeHead(500);
+//                 return res.end('Error loading index.html');
+//             }
+//
+//             res.writeHead(200);
+//             res.end(data);
+//         });
+// }
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+    clientCount++
+    conn.nickname = 'user' + clientCount
+});
+
+
 
 // Scream server example: "hi" -> "HI!!!"
 var server = ws.createServer(function (conn) {
